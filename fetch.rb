de137585b9077c910af97205076b54963b40c7a6
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2012-01-19 20:06:21 -0500

    started on the fetch webpage project

---
+++ fetch.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'mechanize'
+
+agent = Mechanize.new
+
+page = agent.get('http://cli.learncodethehardway.org/book/')
+
+chapter_links = []
+
+page.links.each do |link|
+  link_id = link.attributes['id']
+  if link_id
+    id_split = link_id.split('-')
+    chapter_links << link if id_split[0] == "QQ2" and id_split[1].to_i > 3 
+  end
+end
