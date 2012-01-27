0d7c9ced073853ba4b95e78dce495f2bb58ddfc2
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2012-01-26 20:29:41 -0500

    threw out the pdf printing application and created our first web application using heroku

---
+++ our_first_web_app.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+require 'sinatra'
+
+get '/' do
+  puts params
+  'Hello, please add your name after the slash.'
+end
+
+get '/my-name-is/:name' do
+  "Hello, #{params[:name]}!"
+end
+
+get '/:sender/send/:amount/:currency/to/:receiver' do
+  amount = params[:amount].to_f
+  if amount <= 0
+    return "You need to specify a number more than 0!"
+  end
+  
+  amount = sprintf( "%0.02f", amount)
+   
+  if params[:currency].downcase == 'usd'
+    currency = '$'
+  elsif params[:currency].downcase == 'eur'
+    currency = '€'
+  elsif params[:currency].downcase == 'rub'
+    currency = '₱'
+  elsif params[:currency].downcase == 'uah'
+    currency = '₴'
+  else
+    currency = params[:currency]
+  end
+  
+  "You, #{params[:sender]}, just sent #{currency}#{amount} to #{params[:receiver]}!"
+end
